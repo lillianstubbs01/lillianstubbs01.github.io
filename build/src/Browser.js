edd53523564d9d7600f8534c459b72e51587ce9e
lillianstubbs01 <lillianstubbs01@comcast.net>
2022-07-24 19:26:41 -0500

    starting home

--- build/src/Browser.js
+++ build/src/Browser.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter as Router, Route,} from 'react-router-dom';
+import {HashRouter as Router, Route,} from 'react-router-dom';
 import {Routes} from 'react-router';
 import Home from './routes/Home';
 
